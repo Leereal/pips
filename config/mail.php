d0595cf7278edad86711f4cfea7b099f8c7ea38f
Liberty Mutabvuri <leereal08@ymail.com>
2022-01-30 16:31:37 +0200

    Mail Conf

--- config/mail.php
+++ config/mail.php
@@ -36,11 +36,11 @@ return [
     'mailers' => [
         'smtp' => [
             'transport' => 'smtp',
-            'host' => env('MAIL_HOST', 'smtp.mailgun.org'),
-            'port' => env('MAIL_PORT', 587),
+            'host' => env('MAIL_HOST', 'smtp.mailtrap.io'),
+            'port' => env('MAIL_PORT', 2525),
             'encryption' => env('MAIL_ENCRYPTION', 'tls'),
-            'username' => env('MAIL_USERNAME'),
-            'password' => env('MAIL_PASSWORD'),
+            'username' => env('ddfaa7acd5deaba'),
+            'password' => env('2f55a3cd6275a'),
             'timeout' => null,
             'auth_mode' => null,
         ],
